7e62bc38a908530c0731324f8cbef88785ee8513
pietroiusti <giulio.pietroiusti@gmail.com>
2022-08-17 13:47:21 +0200

    In RoomDetailComponent get room data from service and display it.

--- src/app/room-detail/room-detail.component.ts
+++ src/app/room-detail/room-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
+import { RoomService } from '../room.service';
+
+import { Booking } from '../booking';
 
 @Component({
   selector: 'app-room-detail',
@@ -9,12 +12,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RoomDetailComponent implements OnInit {
   id: number | undefined;
+  bookings: Booking[] = [];
 
   constructor(
     private route: ActivatedRoute,
+    private roomService: RoomService,
   ) { }
 
   ngOnInit(): void {
     this.id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+
+    let data = this.roomService.getRoomData();
+    let r = data.find(r => r.id === this.id);
+    if (r)
+      this.bookings = r.bookings;
   }
 }
