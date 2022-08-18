4ef59d67e561ee43db0d135f1c75d8c26c1600e4
pietroiusti <giulio.pietroiusti@gmail.com>
2022-08-18 17:59:42 +0200

    Check whether booking start time is before end time.

--- src/app/assess-room-booking.service.ts
+++ src/app/assess-room-booking.service.ts
@@ -10,8 +10,33 @@ export class AssessRoomBookingService {
 
   constructor() { }
 
+  endBeforeStart (booking: Booking): boolean {
+    console.log('startBeforeEnd');
+    if ( booking.end < booking.start )
+      return true;
+    else 
+     return false;
+  }  
+
+  // check whether two time frames overlap  
+  overlap(): boolean {
+    //return true;
+    return false;
+  }
+
   assess(roomId: string | undefined, booking: Booking): RoomBookingAssessment {
     console.log('Assessing:')
+
+    if (this.endBeforeStart(booking))
+      return { result: false, msg: "End time cannot be before start time... Are you awake?" };
+
+    if (this.overlap()) {
+      console.log('foobarbaz');
+      return { result: false, msg: "Check current bookings again..." };
+    } 
+
+    console.log('Room: ' + roomId);
+    console.log("booking:");
     console.log(booking);
 
     //todo: logic
