ec87d6c93773b29fc810a713f53ff1d3caba1cc4
pietroiusti <giulio.pietroiusti@gmail.com>
2022-08-23 16:56:42 +0200

    Use onPush change detection strategy in RoomCurrentBookingsComponent.

--- src/app/room-current-bookings/room-current-bookings.component.ts
+++ src/app/room-current-bookings/room-current-bookings.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Booking } from '../models/booking';
 
 @Component({
   selector: 'app-room-current-bookings',
   templateUrl: './room-current-bookings.component.html',
-  styleUrls: ['./room-current-bookings.component.css']
+  styleUrls: ['./room-current-bookings.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoomCurrentBookingsComponent implements OnInit {
 
