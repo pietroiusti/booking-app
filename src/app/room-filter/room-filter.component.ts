d44fe6df1ff643803273400fda0e45960162fda4
pietroiusti <giulio.pietroiusti@gmail.com>
2022-09-01 11:44:58 +0200

    Add room filter component.

---
+++ src/app/room-filter/room-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-filter',
+  templateUrl: './room-filter.component.html',
+  styleUrls: ['./room-filter.component.css']
+})
+export class RoomFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
