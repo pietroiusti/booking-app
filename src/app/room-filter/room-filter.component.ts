8a8c1427f546e2ec51ed8c85353d3f8eebe2431b
pietroiusti <giulio.pietroiusti@gmail.com>
2022-09-01 18:18:26 +0200

    Sketch typeahead filtering methods.

--- src/app/room-filter/room-filter.component.ts
+++ src/app/room-filter/room-filter.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { HtmlParser } from '@angular/compiler';
+import { Component, OnInit, Directive, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+
+import { AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { from, fromEvent, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, tap, switchMap, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-room-filter',
   templateUrl: './room-filter.component.html',
   styleUrls: ['./room-filter.component.css']
 })
-export class RoomFilterComponent implements OnInit {
+export class RoomFilterComponent implements OnInit, AfterViewInit {
+  nameControl = new FormControl('');
+
+  @ViewChild('nameFilter') nameFilter!: ElementRef;
+
+  // DIFFERENT WAYS OF IMPLEMENTING FILTERING BY NAME:
+
+  // FILTERING 1:
+  // Use rxjs' fromEvent to get typeahead$.
+  // Subscribe to typeahead$ and
+  // pass values emitted by typeahead$ to parent (rooms component), which
+  // uses the string value to filter the rooms array displayed in its template
+  @Output() nameFilterEvent: EventEmitter<any> = new EventEmitter();
+
+  // FILTERING 2:
+  // Use rxjs' fromEvent to get typeahead$.
+  // Pass typeahead observalbe to parent (rooms component), which  combines
+  // rooms$ and typeahead$ in order to listen to both of them, and change
+  // the rooms array displayed in the template as soon as new value is emitted
+  // by either rooms$ or typeahead$
+  @Output() nameFilterEvent2: EventEmitter<any> = new EventEmitter();
+
+  // FILTERING 3:
+  // use (input) in the template to pass a value to rooms component
+  // Somehow achieve the same result that we would achieve with fromEvent()
+  @Output() nameFilterEvent3: EventEmitter<any> = new EventEmitter();
+
+  ngAfterViewInit() {
+    console.log(this.nameFilter);
+    console.log(this.nameFilter.nativeElement);
+
+    // create observable for user text input
+    const typeahead = fromEvent(this.nameFilter.nativeElement, 'input').pipe(
+      map(e => ((e as InputEvent).target as HTMLInputElement).value),
+      //filter(text => text.length > 2),
+      //debounceTime(10),
+      distinctUntilChanged()
+    );
+
+    // filtering 2
+    // send typeahead observable to parent
+    this.nameFilterEvent2.emit(typeahead);
+
+    //filtering 1
+    typeahead.subscribe( data => {
+      console.log('typeahead: ' + data);
+      this.nameFilterEvent.emit(data);
+    });
+  }
+
+  //filtering 3
+  handleInputEvent3(event: Event) {
+    console.log('handleInputEvent3()');
+    console.log(event);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@
 import { Location } from '@angular/common';
 
 import { Store } from '../store';
-import { Observable, Subscription } from 'rxjs';
+import { elementAt, Observable, Subscription, map, combineLatest } from 'rxjs';
 
 import { Room } from '../models/room';
 
@@ -14,9 +14,18 @@ import { Room } from '../models/room';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoomsComponent implements OnInit {
+
   rooms: Room[] | null = null;
   rooms$!: Observable<Room[]>;
   subscription!: Subscription;
+
+  //filtering 1 ######
+  filteredRooms: Room[] | null = null;
+
+
+  //filtering 2 ######
+  filteredRooms2: Room[] | null = null;
+
 
   constructor(
     private location: Location,
@@ -27,12 +36,64 @@ export class RoomsComponent implements OnInit {
   ngOnInit(): void {
     this.rooms$ = this.store.select<Room[]>('rooms');
     this.rooms$.subscribe(rooms => {
-      //this.roomsKeys = rooms.map(r => r.id);
       this.rooms = rooms;
+
+      //if (! this.filteredRooms2) {
+      //  console.log(' !this.filteredRooms2');
+      //  this.filteredRooms2 = rooms;
+      //  console.log(rooms);
+      //}
 
       this.cd.markForCheck();// TODO: detectChanges instead? Investigate differences.
     });
+
+    // filtering 1
+    this.rooms$.pipe(elementAt(1)).subscribe(v => this.filteredRooms = v);
   }
+
+  // filtering 1 ######
+  // receives values emitted by typeahead$
+  // change room list displayed in template accordingly
+  handleNameFilterEvent(data: string) {
+    console.log('handleNameFilterEvent()');
+    console.log(data);
+    // update view showing only filtered rooms
+    if (this.rooms) {
+      let re = new RegExp(data, 'i');
+      //this.rooms = this.rooms.filter( r => re.test(r.name) );
+      this.filteredRooms = this.rooms.filter( r => re.test(r.name) );
+    }
+  }
+
+  // filtering 2 ######
+  // Receive typeahead$
+  // combine typeahead$ with room$ using combineLastest()
+  // display filtered room
+  handleNameFilterEvent2(typeahead$: Observable<string>) {
+    console.log('handleNameFilterEvent2()');
+
+    console.log(this.filteredRooms2);
+
+    let combined = combineLatest([this.rooms$, typeahead$]);
+
+    combined.subscribe(val => {
+      let rooms = val[0];
+      let filterString = val[1];
+
+      let re = new RegExp(filterString, 'i');
+      this.filteredRooms2 = rooms.filter( r => re.test(r.name) );
+    })
+  }
+
+  // filtering 3 ######
+  // Receive inputEvent
+  // combine typeahead$ with room$ using combineLastest()
+  // display filtered room
+  handleNameFilterEvent3() {
+    console.log('handleNameFilterEvent3()');
+  }
+
+
 
   logStore() {
     console.log(this.store);
